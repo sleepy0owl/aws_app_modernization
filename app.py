97480dcdc72e217eaf1d0c436f0016cb117f1963
sleepy0owl <souravcristiano502@gmail.com>
2022-08-10 15:44:06 +0530

    Fixing env value

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ MicroserviceEcsStack(app, "MicroserviceEcsStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    env=cdk.Environment(account='960351580303', region='us-east-1'),
+    # env=cdk.Environment(account='111111111111', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
