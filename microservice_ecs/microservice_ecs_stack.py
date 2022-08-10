8e3eae11e9428e0acbd66b9b7a08d2c46d328df7
sleepy0owl <souravcristiano502@gmail.com>
2022-08-10 15:47:53 +0530

    Add formating

--- microservice_ecs/microservice_ecs_stack.py
+++ microservice_ecs/microservice_ecs_stack.py
@@ -10,17 +10,24 @@ class MicroserviceEcsStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
+
         # ! importing VPC
         self.vpc = ec2.Vpc.from_lookup(
-            self, "monolith_stack_vpc", vpc_id='vpc-02286ac3ed54a0400')
+            self, "monolith_stack_vpc",
+            vpc_id='vpc-02286ac3ed54a0400'
+        )
 
         # ! creating cluster
         self.cluster = ecs.Cluster(
-            self, "unicorn-shop-ecs-cluster", vpc=self.vpc)
+            self, "unicorn-shop-ecs-cluster",
+            vpc=self.vpc
+        )
 
         # ! importing security group
-        self.web_server_sg = ec2.SecurityGroup.from_security_group_id(self, "webserver_sg", security_group_id="sg-0a39f7eb07425a7e8")
+        self.web_server_sg = ec2.SecurityGroup.from_security_group_id(
+            self, "webserver_sg",
+            security_group_id="sg-0a39f7eb07425a7e8"
+        )
 
         # ! creating auto scaling group
         auto_scaling_group = autoscaling.AutoScalingGroup(
@@ -35,23 +42,32 @@ class MicroserviceEcsStack(Stack):
             key_name="lnd_hobbit_db_key",
             desired_capacity=1
         )
-        capacity_provider = ecs.AsgCapacityProvider(self, "capacity_provider", auto_scaling_group=auto_scaling_group)
+        capacity_provider = ecs.AsgCapacityProvider(
+            self,
+            "capacity_provider",
+            auto_scaling_group=auto_scaling_group
+        )
         self.cluster.add_asg_capacity_provider(provider=capacity_provider)
-        
+
         # ! creating task definition
-        self.ec2_task_definition = ecs.Ec2TaskDefinition(self, 'unicorn_shop_task_def')
+        self.ec2_task_definition = ecs.Ec2TaskDefinition(
+            self, 'unicorn_shop_task_def'
+        )
         # ! adding public registry
-        container = self.ec2_task_definition.add_container("unicron_shop_container", 
-                                                            image=ecs.ContainerImage.from_registry("public.ecr.aws/w0l8v3b9/app_modernization_workshop"),
-                                                            memory_limit_mib=512
-                                                        )
+        container = self.ec2_task_definition.add_container(
+            "unicron_shop_container",
+            image=ecs.ContainerImage.from_registry(
+                "public.ecr.aws/w0l8v3b9/app_modernization_workshop"),
+            memory_limit_mib=512
+        )
 
         # ! adding port mapping
         container.add_port_mappings(
             PortMapping(container_port=8080, host_port=8080))
 
         # ! creating service
-        self.service = ecs.Ec2Service(self, "unicorn_ec2_service",
-                                        task_definition=self.ec2_task_definition,
-                                        cluster=self.cluster
-                                    )
+        self.service = ecs.Ec2Service(
+            self, "unicorn_ec2_service",
+            task_definition=self.ec2_task_definition,
+            cluster=self.cluster
+        )
